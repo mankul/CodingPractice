647780e4fe842689618c932f2113c97155ced6f8
mankul <mankulgupta@gmail.com>
2024-06-30 23:34:17 +0530

    Intermediate task

--- DYNAMIC_PROGRAMMING/fib_series.cpp
+++ DYNAMIC_PROGRAMMING/fib_series.cpp
@@ -1,29 +1,43 @@
 #include<iostream>
-
+#include<malloc.h>
 // not tested yet.
 using namespace std;
 
 
-int  fib(int num, int ** arr, int & len){
-	if num == 1 or num ==2:
-		*arr[num] = 1;
-		len = num;
-		return 1;
-	if len > num
-		return *arr[num];
-	if num == len 
-		arr[num] = arr[len-2] + arr[len-1];
-		len += 1;
-		return *arr[num];
+int  fib(int pos_num, int ** arr, int & arr_populated){
+	if(pos_num == 1 or pos_num ==2){
+		(*arr)[pos_num] = 1;
+		arr_populated = pos_num;
+	}
+	else if(arr_populated > pos_num)
+		return (*arr)[pos_num];
+	else if(pos_num == arr_populated){
+		(*arr)[pos_num] = (*arr)[arr_populated-2] + (*arr)[arr_populated-1];
+		arr_populated += 1;
+	}
 	else
-		int num1  = fib(num - 1, arr, len);
-		int num2 = fib(num - 2, arr, len)
-		return num1 + num2;
+	{
+		int left_fib  = fib(pos_num - 1, arr, arr_populated);
+		int right_fib  = fib(pos_num - 2, arr, arr_populated);
+		(*arr)[pos_num] = left_fib + right_fib;
+	}
+	return (*arr)[pos_num];
+}
+
+void display_arr(int *arr, int len){
+	int index;
+	for(index = 0; index < len; index++){
+		cout<<arr[index];
+	}
 }
 
 
 int main(){
-	int *arr;
-
+	int **arr;
+	int pos_num;
+	cin>>pos_num;
+	*arr = (int *)malloc(n * sizeof(int));
+	int arr_populated = 0;
+	int end_pos_num = fib(pos_num, arr, arr_populated);
 	return 0;
 }
