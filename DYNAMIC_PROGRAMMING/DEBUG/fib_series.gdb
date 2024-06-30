0b873c53cdc0f819c143beba1bdec97fd4a595bb
mankul <mankulgupta@gmail.com>
2024-06-30 23:49:03 +0530

    intermeduate dyn prog fib series

---
+++ DYNAMIC_PROGRAMMING/DEBUG/fib_series.gdb
@@ -0,0 +1,1 @@
+b fib_series.cpp:main()
--- DYNAMIC_PROGRAMMING/fib_series.cpp
+++ DYNAMIC_PROGRAMMING/fib_series.cpp
@@ -4,21 +4,13 @@
 using namespace std;
 
 
-int  fib(int pos_num, int ** arr, int & arr_populated){
+int  fib(int pos_num, int ** arr){
 	if(pos_num == 1 or pos_num ==2){
 		(*arr)[pos_num] = 1;
-		arr_populated = pos_num;
 	}
-	else if(arr_populated > pos_num)
-		return (*arr)[pos_num];
-	else if(pos_num == arr_populated){
-		(*arr)[pos_num] = (*arr)[arr_populated-2] + (*arr)[arr_populated-1];
-		arr_populated += 1;
-	}
-	else
-	{
-		int left_fib  = fib(pos_num - 1, arr, arr_populated);
-		int right_fib  = fib(pos_num - 2, arr, arr_populated);
+	else if( (*arr)[pos_num] == -1){
+		int left_fib  = fib(pos_num - 1, arr);
+		int right_fib  = fib(pos_num - 2, arr);
 		(*arr)[pos_num] = left_fib + right_fib;
 	}
 	return (*arr)[pos_num];
@@ -31,13 +23,17 @@ void display_arr(int *arr, int len){
 	}
 }
 
+void initialize_arr(int ** arr, int len){
+	for(int i = 0; i < len; i++)
+		(*arr)[i] = -1;
+}
 
 int main(){
 	int **arr;
 	int pos_num;
 	cin>>pos_num;
-	*arr = (int *)malloc(n * sizeof(int));
-	int arr_populated = 0;
-	int end_pos_num = fib(pos_num, arr, arr_populated);
+	*arr = (int *)malloc(pos_num * sizeof(int));
+	initialize_arr(arr, pos_num);
+	int end_pos_num = fib(pos_num, arr);
 	return 0;
 }
