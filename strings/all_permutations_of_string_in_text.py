b4483e02d76ede1dba0dc11e0c48ae1f125b497a
mankul <mankulgupta@gmail.com>
2024-08-19 23:34:53 +0530

    sliding window psuedocode is good, need to verify the python programs

--- strings/all_permutations_of_string_in_text.py
+++ strings/all_permutations_of_string_in_text.py
@@ -16,6 +16,45 @@ windows = len(text) - len(string)  + 1
 substrings_ = []
 substr_counts = 0
 
+
+# sliding windows to find all possible substring permutations.
+
+index = 0
+subindex = 0
+char_dict_copy = char_dict.copy()
+count = 0
+
+while index < windows:
+    while subindex < len(string):
+        if text[index + subindex] in char_dict_copy.keys():
+            char_dict_copy[text[index + subindex]] -= 1
+            if char_dict_copy[text[index + subindex]] == 0:
+                char_dict_copy.pop(text[index + subindex])
+            count += 1
+            subindex += 1
+        else:
+            break
+    if count == len(string):
+        substr_counts += 1
+        substrings_.append(text[index:index + count])
+
+        char_dict_copy[text[index]] = 1
+        count -= 1
+
+        index += 1
+        subindex -= 1
+    else:
+        count = 0
+        char_dict_copy = char_dict.copy()
+
+        index += subindex
+        subindex = 0
+
+
+
+
+# naive strategy
+# all substrings are compared.
 for index in range(windows):
     count = 0
     char_dict_copy = char_dict.copy()
