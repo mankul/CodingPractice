5f39137c5f087c2cbd0b5769004024bde7a53ce4
mankul <mankulgupta@gmail.com>
2024-08-24 23:18:03 +0530

    doc for all primes in code itself. Code is running and producing correct result. Is there a better way to increment the loop?

--- prime/allPrimes.cpp
+++ prime/allPrimes.cpp
@@ -5,6 +5,15 @@
 //using namespace std;
 
 
+// Aim: is to identify all the prime numbers below the given number
+// Algo: To find all the numbers, let us grow loop with 2 increment. If the number is a prime, it shall not be divisible
+// by all the prime numbers smaller than itself. Hence using a dynamic array to store all prime numbers. The numbers
+// will be stored in ascending order itself. 
+// Is there any better way to increment the loop? This increment is making finding the algorithm very slow."
+// Complexity: 
+// Time: for every test case, finding the primes with 2 step incr. Therefore O(n)
+// space: Depends upon the count of primes smaller than given number . \theta(logn). Need to verify.
+
 void allPrimes(int num, std::vector<int> & arr){
     int counter = 2;
     bool prime_flag;
